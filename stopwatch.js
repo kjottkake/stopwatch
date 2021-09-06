6971cfb255cc0840984024c14434a851ddb60720
kjottkake <38797885+kjottkake@users.noreply.github.com>
2021-09-06 12:28:29 +0200

    added comments

--- stopwatch.js
+++ stopwatch.js
@@ -1,39 +1,44 @@
+//selects stopwatch ui element
 const watch = document.querySelector("#stopwatch");
-      let millisecound = 0;
-      let timer;
 
-      function timeStart(){
-        watch.style.color = "#0f62fe";
-        clearInterval(timer);
-        timer = setInterval(() => {
-          millisecound += 10;
+//defines variables
+let millisecound = 0;
+let timer;
 
-          let dateTimer = new Date(millisecound);
+//start time function
+function timeStart() {
+    watch.style.color = "#0f62fe";
+    clearInterval(timer);
+    timer = setInterval(() => {
+        millisecound += 10;
 
-          watch.innerHTML = 
-          ('0'+dateTimer.getUTCHours()).slice(-2) + ':' +
-          ('0'+dateTimer.getUTCMinutes()).slice(-2) + ':' +
-          ('0'+dateTimer.getUTCSeconds()).slice(-2) + ':' +
-          ('0'+dateTimer.getUTCMilliseconds()).slice(-3,-1);
-        }, 10);
-      }
+        let dateTimer = new Date(millisecound);
+
+        watch.innerHTML =
+            ('0' + dateTimer.getUTCHours()).slice(-2) + ':' +
+            ('0' + dateTimer.getUTCMinutes()).slice(-2) + ':' +
+            ('0' + dateTimer.getUTCSeconds()).slice(-2) + ':' +
+            ('0' + dateTimer.getUTCMilliseconds()).slice(-3, -1);
+    }, 10);
+}
 
 
-      function timePaused() {
-        watch.style.color = "red";
-        clearInterval(timer);
-      }
+//pause time function
+function timePaused() {
+    watch.style.color = "red";
+    clearInterval(timer);
+}
 
-      function timeReset(){
-        setInterval(timer)
-        millisecound = 0;
-        watch.innerHTML = "00:00:00:00";
-      }
+function timeReset() {
+    setInterval(timer)
+    millisecound = 0;
+    watch.innerHTML = "00:00:00:00";
+}
 
-      document.addEventListener('click', (e) => {
-        const el = e.target;
+document.addEventListener('click', (e) => {
+    const el = e.target;
 
-        if(el.id === 'start') timeStart();
-        if(el.id === 'pause') timePaused();
-        if(el.id === 'reset') timeReset();
-      })
+    if (el.id === 'start') timeStart();
+    if (el.id === 'pause') timePaused();
+    if (el.id === 'reset') timeReset();
+})
